46ad6446f3718aae5389d1776e828856a8f2b006
Kostya Kan <kostikkan595@gmail.com>
2022-11-29 23:32:44 +0500

    fix: Removed not using vars

--- src/client/pages/Logpage.tsx
+++ src/client/pages/Logpage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import FormLog from '../components/Logform';
-import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
 
 export default function Logpage() {
   return (
--- src/client/pages/Regpage.tsx
+++ src/client/pages/Regpage.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import FormReg from '../components/Regform';
-import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
 
 export default function Regpage() {
   return (
